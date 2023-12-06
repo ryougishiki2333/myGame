51708d4e21a877f114412bce94b14c8701fae040
liaotingwei <ryougishiki23333@gmail.com>
2023-12-06 11:24:01 +0800

    init main

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,20 @@ const App: React.FC = () => {
 
   const renderGrid = () => {
     const grid = [];
-
-    for (let row = 0; row < gridSize; row++) {
-      for (let col = 0; col < gridSize; col++) {
-        const cellId = `cell-${row}-${col}`;
-        grid.push(<div key={cellId} className="cell" id={cellId}></div>);
-      }
+  
+    for (let i = 0; i < gridSize * gridSize; i++) {
+      const cellId = `cell-${i}`;
+      grid.push(<div key={cellId} className="cell" id={cellId}></div>);
     }
-
+  
     return grid;
   };
 
   return (
-    <div className="App">
+    <div className="main">
+      <div className="side"></div>
       <div className="grid">{renderGrid()}</div>
+      <div className="side"></div>
     </div>
   );
 };
